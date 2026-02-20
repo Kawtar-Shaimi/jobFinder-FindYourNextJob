d09fb1f0ac0e0746e5865df6ebddad3f42816018
Kawtar Shaimi <kawtar@jobfinder.dev>
2026-02-20 17:36:15 +0000

    feat(applications): add ApplicationService for CRUD with JSON Server

---
+++ src/app/core/services/application.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Application } from '../models/application.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApplicationService {
+    private apiUrl = 'http://localhost:3000/applications';
+
+    constructor(private http: HttpClient) { }
+
+    getApplicationsByUser(userId: number): Observable<Application[]> {
+        return this.http.get<Application[]>(`${this.apiUrl}?userId=${userId}`);
+    }
+
+    addApplication(application: Application): Observable<Application> {
+        return this.http.post<Application>(this.apiUrl, application);
+    }
+
+    updateApplication(id: number, data: Partial<Application>): Observable<Application> {
+        return this.http.patch<Application>(`${this.apiUrl}/${id}`, data);
+    }
+
+    deleteApplication(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+}
