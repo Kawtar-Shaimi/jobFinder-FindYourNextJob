f27cc3262c5c10fed4d5d5b600dc9b69e651660b
Kawtar Shaimi <kawtar@jobfinder.dev>
2026-02-22 13:51:33 +0000

    feat(config): add development environment configuration file

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    jsonServerUrl: 'http://localhost:3000',
+    adzuna: {
+        appId: 'your_adzuna_app_id',
+        apiKey: 'your_adzuna_api_key',
+        baseUrl: 'https://api.adzuna.com/v1/api/jobs'
+    }
+};
