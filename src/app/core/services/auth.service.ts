9246df4df5b7a05ec143dd83ff215a82fc42a029
Kawtar Shaimi <kawtar@jobfinder.dev>
2026-02-22 14:00:33 +0000

    refactor(auth): use environment config for API URL, standardize indentation

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, switchMap } from 'rxjs/operators';
 import { User, AuthUser } from '../models/user.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    private apiUrl = 'http://localhost:3000/users';
+    private apiUrl = `${environment.jsonServerUrl}/users`;
     private storageKey = 'jobfinder_user';
 
     constructor(private http: HttpClient) { }
