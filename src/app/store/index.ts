3f6ceec6e698a64609a54233a72bdaaddd80727c
Kawtar Shaimi <kawtar@jobfinder.dev>
2026-02-22 14:02:58 +0000

    chore(ngrx): add store barrel index.ts for clean imports

---
+++ src/app/store/index.ts
@@ -0,0 +1,4 @@
+﻿export * from './favorites/favorites.actions';
+export * from './favorites/favorites.reducer';
+export * from './favorites/favorites.selectors';
+export * from './favorites/favorites.effects';
